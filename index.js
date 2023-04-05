b5d8c416e7bd9b3a80675ede1f843c610865e325
Barbara Laurince <blaurince@hotmail.com>
2023-04-05 14:44:39 -0400

    Added delete and update
    Bug Fix update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { getAllPlants, addPlant } from "./src/plants.js"
+import { getAllPlants, addPlant, deletePlant, updatePlant } from "./src/plants.js"
 
 const PORT = 3001;
 
@@ -9,8 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
 app.get("/plants", getAllPlants) ;
-app.post("/plants", addPlant)
+app.post("/plants", addPlant);
+app.delete("/plants/:docId", deletePlant);
+app.patch("/plants/:docId", updatePlant);
 
 //api points / routes
 
--- src/plants.js
+++ src/plants.js
@@ -1,17 +1,39 @@
+// import { ObjectId } from "mongodb";
 import { db } from "./connectDB.js";
+import { ObjectId } from "mongodb";
+
 const coll = db.collection("plants");
 
+// CRUD: GET
 export async function getAllPlants(req, res){
     const plants = await coll.find({}).toArray();
     res.send(plants);
 }
 
-
-export async function addPlant(req,res){
+// CRUD: POST
+export async function addPlant(req,res) {
     const newPlant = req.body;
     await coll.insertOne(newPlant);
     res.status(201).send({ message: "new plant added " });
-    
+} 
+// CRUD: DELETE
+ export async function deletePlant(req, res){
+     const docId = { "_id": new ObjectId(req.params.docId) };
 
+    await coll.deleteOne(docId);
+    res.status(201).send( { message: "plant had been sadly deleted" } );
+ 
+}
 
+// Crud : update
+export async function updatePlant(req, res){
+
+    const docId = { "-id": new ObjectId(req.params.docId)};
+    const updatePlant = req.body;
+
+   await coll.findOneAndUpdate(
+    {"_id" : docId},
+    { $set: { updatePlant} }
+   );
+   res.status(201).send( { message: "plant has been updated"});
 }
